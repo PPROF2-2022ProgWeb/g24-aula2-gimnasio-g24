43b2b91af8e6c537ed641e5333dac6688529709c
gustavog <grgodoy1984@live.com>
2021-10-24 20:42:33 -0300

    pruebas con js

--- js/archivo.js
+++ js/archivo.js
@@ -1,2 +1,17 @@
 function pruebaExterna(){
 	alert('al actualizar Un mensaje de prueba desde un archivo externo');}
+
+	/*document.getElementById('btn_alerta').onclick = function(){
+		alert('Hola Mundo!');*/
+		//declaraciones de variables
+		var_numero = 100;
+		var numero = 100;
+		let $numero = 100;
+		let letNumero=100;
+		const numeroPar = 100;
+		const PI = 3.14;
+
+		if (true) {
+			var x = 5;
+		  }
+		  console.log(x); // x vale 5		  
