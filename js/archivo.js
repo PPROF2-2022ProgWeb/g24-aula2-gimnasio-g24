496699d59dbf7f96cb38698bcd9b5f38350b1e59
gustavog <grgodoy1984@live.com>
2021-11-03 20:42:08 -0300

    Correcion index en rama gustavog

--- js/archivo.js
+++ js/archivo.js
@@ -1,14 +1,2 @@
 function pruebaExterna(){
 	alert('al actualizar Un mensaje de prueba desde un archivo externo');}
-//pruebas
-document.getElementById('d').innerHTML()='¡Hola Mundo!';
-
-function controlIngreso(){
-	
-				if($u == "" || $_POST['clave'] == null){ // Validamos que ningún campo quede vacío
-        alert('INGRESASTE!!'); // Se utiliza Javascript dentro de PHP
-    }else{
-	window.location.href = "http://planidear.com.ar/a5-g6-gimnasio/form_ingreso.php";
-	}
-	
-}
