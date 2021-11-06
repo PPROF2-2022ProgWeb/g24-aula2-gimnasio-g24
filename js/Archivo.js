05e191685fc6610e3fb5bd8c38059ef4db7ed4cf
Alfredo Nuñez <alfre0688@hotmail.com>
2021-11-06 13:00:08 -0300

    se agregó codigo para trabajar con la pagina contacto

--- js/Archivo.js
+++ js/Archivo.js
@@ -1,2 +1,34 @@
-function pruebaExterna(){
-	alert('al actualizar Un mensaje de prueba desde un archivo externo');}
+window.addEventListener("load", inicio);
+
+function inicio() {
+    console.log("Que hace nene");
+    //window.addEventListener("click", envioMensaje(evento));
+    document.getElementById("btn-enviar").addEventListener("click", envioMensaje);
+    document.getElementById("btn-volver").addEventListener("click", volverAInicio);
+}
+
+function pruebaExterna() {
+    alert('al actualizar Un mensaje de prueba desde un archivo externo');
+}
+
+function envioMensaje(e) {
+    e.preventDefault();
+    console.log("HOLA");
+    let input_nombre = document.getElementById('input_nombre');
+    let input_correo = document.getElementById('input_correo');
+    let input_textarea = document.getElementById('input_textarea');
+    if (input_nombre.value == "") {
+        input_nombre.value = "Este campo no puede estar vacio";
+    }
+    if (input_correo.value == "") {
+        input_correo.value = "Este campo no puede estar vacio";
+    }
+    if (input_textarea.value == "") {
+        input_textarea.value = "Este campo no puede estar vacio";
+    }
+
+}
+
+function volverAInicio() {
+    window.location = "index.html";
+}
