9a59db756bfed3a591440a6777aab434a97a578b
gustavog <gustavog@live.com.ar>
2022-07-03 19:33:43 -0300

    act pag contacto/ ecommerce/ login/ ayuda

--- appGym/src/app/Page/app-routing.module.ts
+++ appGym/src/app/Page/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import  { HomeComponent } from '../app/Page/home/home.component';
-import  { ContactoComponent } from '../app/Page/contacto/contacto.component';
-import  { EquipoComponent } from '../app/Page/equipo/equipo.component';
-import  { LoginComponent } from '../app/Page/login/login.component';
-import  { AyudaComponent } from '../app/Page/ayuda/ayuda.component';
-import {PageNotFoundComponent} from '../app/Page/PageNotFound/PageNotFound.component';
-import {EcommerceComponent } from '../app/Page/ecommerce/ecommerce.component';
+import  { HomeComponent } from '../Page/home/home.component';
+import  { ContactoComponent } from '../Page/contacto/contacto.component';
+import  { EquipoComponent } from '../Page/equipo/equipo.component';
+import  { LoginComponent } from '../Page/login/login.component';
+import  { AyudaComponent } from '../Page/ayuda/ayuda.component';
+import {PageNotFoundComponent} from '../Page/PageNotFound/PageNotFound.component';
+import {EnvCorreoComponent } from '../Page/EnvCorreo/EnvCorreo.component';
+
 
 const routes: Routes = [
 
@@ -15,7 +16,8 @@ const routes: Routes = [
 { path: 'equipo', component: EquipoComponent},
 { path: 'login', component: LoginComponent},
 { path: 'ayuda', component: AyudaComponent},
-{ path: 'ecommerce', component: EcommerceComponent},
+{ path: 'EnvCorreo', component: EnvCorreoComponent},
+{ path: 'envCorreo', component: EnvCorreoComponent},
 
 /* re direcciona al inicio */
 {path: '', redirectTo: '/home', pathMatch: 'full'},
